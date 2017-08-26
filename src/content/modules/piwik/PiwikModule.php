2e9efb8e0b17149295246ef4baa6d75fc1dd9474
Ulrich Schmidt <admin@deruli.de>
2017-08-26 09:02:51 +0200

    /piwik.php wird von ende der url entfernt

--- src/content/modules/piwik/PiwikModule.php
+++ src/content/modules/piwik/PiwikModule.php
@@ -16,11 +16,11 @@ class PiwikModule extends Controller
         if (Request::isPost() and $acl->hasPermission(getModuleMeta($this->moduleName, "admin_permission"))) {
             $normalizedURL = ltrim(Request::getVar("piwik_url"), "http://");
             $normalizedURL = ltrim($normalizedURL, "https://");
+            $normalizedURL = rtrim($normalizedURL, "/piwik.php");
             $normalizedURL = rtrim($normalizedURL, "/");
             Settings::set("piwik_url", $normalizedURL, "str");
             Settings::set("piwik_site_id", Request::getVar("piwik_site_id", null, "int"));
         }
-        
         return Template::executeModuleTemplate($this->moduleName, "settings.php");
     }
 }
